bdbcdd7b66e208c46458ec74bb2a31c5867c7bdb
fbinder <binder@kull.ch>
2026-02-02 13:01:51 +0100

    remove mockup viewer and embrace css-vars

--- wordle-frontend/src/app/app.routes.ts
+++ wordle-frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
-import { MockupViewerComponent } from './mockup-viewer/mockup-viewer.component';
+
 
 export const routes: Routes = [
-    { path: '', component: MockupViewerComponent }
+
 ];
